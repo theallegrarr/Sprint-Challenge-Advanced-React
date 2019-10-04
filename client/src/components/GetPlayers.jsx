6fe581ca242dca23c9601dd7692fc82a65ad11a8
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-04 08:52:14 +0100

    class component built to get players

---
+++ client/src/components/GetPlayers.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PlayerCard from './PlayerCard';
+
+const playersApi = 'http://localhost:5000/api/players';
+
+export default class Market extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+    };
+  }
+ 
+  componentDidMount(){
+   const getPlayers = axios.get(playersApi);
+    
+    Promise.all([getPlayers])
+     .then(([playerRes]) => {
+       this.setState({
+         players: playerRes.data,
+       });
+     });
+  }
+ 
+  render() {
+    return (
+    <>
+     <PlayerCard players={this.state.players}/>
+    </>
+    )
+  }
+ }
