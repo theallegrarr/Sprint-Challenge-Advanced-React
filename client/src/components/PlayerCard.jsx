cf961766dd62fa04fa3cfbf0896e4de2e19100ac
Nurudeen <nurudeen.amedu@gmail.com>
2019-10-04 10:10:49 +0100

    design updated

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -8,7 +8,7 @@ export default function (props) {
   <div className={mode ? 'p-cards' : 'p-cards toggledp'}>
     <div className='nav'>
       <h2 data-testid='player-head'>Players List</h2>
-      <div className="dark-mode__toggle">
+      <div className={mode ? "dark-mode__toggle __toggled" : "dark-mode__toggle"}>
       <div
         className={mode ? 'toggle toggled' : 'toggle'}
         onClick={setMode}
